ad1b082bfd52ed589fcfffbe20ad39ce4b501750
Alexander Wu <alex606@att.net>
2017-09-09 23:28:04 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
+
 const App = () => {
     return (
       <div>
           <Header/>
           <Main />
+          <Footer />
         </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div id='footer'>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/alexander-wu-a9a44179/">LinkedIn</a>        
+                </li>
+                <li>
+                    <a href="https://github.com/alex606">GitHub</a>
+                </li>
+            </ul>
+        </div>
+    );
+  }
+
+export default Footer;
