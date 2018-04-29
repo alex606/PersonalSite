4af08d812b9cf0a2a02348d3514fb79acf8187ec
Alex Wu <alexwu606@gmail.com>
2018-04-29 11:37:18 -0400

    Added requried attribute for contact form

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -24,6 +24,7 @@ class TextInput extends Component {
           type={this.state.type}
           name={this.state.name}
           id={this.state.id}
+          required
         />
       </div>);
   }
